50fe8edc358b64a35557a2fdde22652688aae301
danoosit <danoosit@gmail.com>
2024-09-08 21:56:07 +0700

    remove unused code

--- src/App.js
+++ src/App.js
@@ -12,23 +12,6 @@ import HomePage from './pages/Home';
 
 function App() {
 
-  const LandingPage = <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>;
-
   const router = createBrowserRouter([
     {
       path: "*", element: <Root />
