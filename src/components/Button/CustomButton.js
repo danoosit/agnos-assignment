39efa9a86e7ddce5da0bd884b5ce27660f22f376
danoosit <danoosit@gmail.com>
2024-09-08 19:52:00 +0700

    [fix] component style

--- src/components/Button/CustomButton.js
+++ src/components/Button/CustomButton.js
@@ -1,6 +1,6 @@
 function CustomButton(props) {
     const { title, disabled } = props;
-    return <button className="max-w-xl w-screen py-2 px-4 rounded mt-4 border"
+    return <button className="max-w-xl w-full py-2 px-4 rounded mt-4 border"
         style={{
             background: disabled ? '#ebebeb' : '#074E9f',
             color: disabled ? '#c7c7c7' : 'white',
--- src/components/Container/CustomContainer.js
+++ src/components/Container/CustomContainer.js
@@ -1,5 +1,5 @@
 function CustomContainer(props) {
-    return <div className="flex flex-col items-center w-screen" {...props} />
+    return <div className="flex flex-col items-center w-screen p-4" {...props} />
 }
 
 export default CustomContainer;
